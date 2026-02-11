d33c76bf7be014e5fb4e55eadb3eef01a521de9c
Caleb Gomes Pinto <caleb.gp.dev@gmail.com>
2026-02-11 12:21:33 +0000

    feat: 1087

--- 1087/1087.py
+++ 1087/1087.py
@@ -2,3 +2,10 @@ while True:
     x1, y1, x2, y2 = map(int, input().split())
     if x1 == 0:
         break
+    if x1 == x2 and y1 == y2:
+        print(0)
+        continue
+    elif x1 == x2 or y1 == y2 or x1 + x2 == y1 + y2:
+        print(1)
+    else:
+        print(2)
